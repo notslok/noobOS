75ab04ce2630724b18bf325f01c36535dc936ce8
slok <shlokjha.3@gmail.com>
2024-12-08 02:09:15 +0530

    stack pointer address fix

--- boot.s
+++ boot.s
@@ -1,17 +1,17 @@
 .section .text
 .global start
 
+// Logic to make sure that kerne runs only on CPU0 of Rpi
 start:
-@ LOgic to make sure that kerne runs only on CPU0 of Rpi
-    mrs x0, mpidr_el1 @ Multiprocessor Affinity Register -> a system register whose lowest 2 bits, if unset ... indicates CPU0
-    and x0, x0, #3 @ Checking if the lowest 2 bits are unset by performing AND with 3 (0011 ... in binary)
+    mrs x0, mpidr_el1 // Multiprocessor Affinity Register -> a system register whose lowest 2 bits, if unset ... indicates CPU0
+    and x0, x0, #3 // Checking if the lowest 2 bits are unset by performing AND with 3 (0011 ... in binary)
     cmp x0, #0
-    beq kernel_entry @ if CPU0 then branch to <kernel_entry:> section
+    beq kernel_entry // if CPU0 then branch to <kernel_entry:> section
 
 end:
-    b end @ infinite loop in case of condition failure
+    b end // infinite loop in case of condition failure
 
 kernel_entry:
-    mov sp, #80000 @ move the stack pointer to this address as this is the addres from where Rpi expects the kernel code to be loaded in main memory 
-    bl kMain @ branches to KMain (short for kernel main) function but with link.... i.e it update the the LR(link register) with address of the next instruction (i.e. address f instruction "b end")
+    mov sp, #0x80000 // move the stack pointer to this address as this is the addres from where Rpi expects the kernel code to be loaded in main memory 
+    bl KMain // branches to KMain (short for kernel main) function but with link.... i.e it update the the LR(link register) with address of the next instruction (i.e. address f instruction "b end")
     b end
